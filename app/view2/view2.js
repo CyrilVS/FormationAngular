49f3d9ffd5f6bb843be72c6e49e7022a38d51b22
CyrilVS <you@example.com>
2017-12-14 10:53:52 +0100

    Version en cours de travail

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,6 +9,40 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', ['$scope', function($scope) {
+.controller('View2Ctrl', ['$scope', '$http', function($scope, $http) {
+  $scope.recupInfo = function () {
+    $http({
+      url: 'models/listcredits.json',
+      method: 'GET'
+    }).then(function successCallback(response) {
+      $scope.DocRecup = response.data;
+      console.log($scope.DocRecup);
+    }, function errorCallback(response) {
+      console.log(response);
+    });
+  };
+}])
 
-}]);
+.directive('formu', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'widgets/formulaire.html'
+  };
+})
+
+.directive('menuList', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'widgets/menu.html'
+  };
+});
+
+/*.directive('crediteurList', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'widgets/crediteur.html',
+    scope: {
+      personnes: '=DocRecup'
+    }
+  };
+});*/
